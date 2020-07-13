e0a2c5a421dd4bb51a07d3e3e5ac408146fa1732
igncampa <icampabadals@gmail.com>
2020-07-12 20:09:44 -0600

    added article page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-
+from wagtail.core import blocks
 from wagtail.core.models import Page
-
+from wagtail.core.fields import StreamField
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 
 class HomePage(Page):
     pass
+
+class ArticlePage(Page):
+
+    class Meta:
+        verbose_name = "Standard Article"
+
+    body = StreamField([
+        ('html', blocks.RawHTMLBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock(
+            icon="media",
+        )),
+        ('paragraph', blocks.RichTextBlock(
+            icon="pilcrow"
+        )),
+    ])
